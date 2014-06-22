c0c4bd981179261988129af227c47056836e6120
Alan Bondarchuk <imacoda@gmail.com>
2014-06-23 00:45:38 +0600

    Add player default configuration, checkpoint "alpha-1"

--- theme/uppod.tpl.php
+++ theme/uppod.tpl.php
@@ -15,12 +15,17 @@
  */
 ?>
 <div class="uppod-video">
-  <video id="<?php print $html_id ?>" width="<?php print $width ?>" height="<?php print $height ?>" controls="controls" preload="none"<?php if(isset($poster)) : ?> poster="<?php print $poster ?>"<?php endif ?>>
-    <source src="<?php print $file_url ?>"<?php if (isset($file_mime)): ?> type="<?php print $file_mime ?>"<?php endif ?> />
+  <video id="<?php print $html_id ?>" width="<?php print $width ?>" height="<?php print $height ?>" controls="controls"
+         preload="none"<?php if (isset($poster)) : ?> poster="<?php print $poster ?>"<?php endif ?>>
+    <source
+      src="<?php print $file_url ?>"<?php if (isset($file_mime)): ?> type="<?php print $file_mime ?>"<?php endif ?> />
   </video>
 </div>
-<?php if(isset($uppod_inline_js_code)): ?>
-  <script type="text/javascript">
-    //uppod('<?php print $html_id ?>').setup(<?php print $uppod_inline_js_code?>);
-  </script>
-<?php endif ?>
+
+<script type="text/javascript">
+  var config = Drupal.settings.uppod['<?php print $html_id ?>'];
+  var flashvars = {m: "player", file: config.file, st: '/sites/all/libraries/uppod/st/zvezda.skin', poster: "<?php print $poster; ?>"};
+
+  var params = {bgcolor: "#000", wmode: "window", allowFullScreen: "true", allowScriptAccess: "always"};
+  swfobject.embedSWF(config.flashplayer, "<?php print $html_id ?>", config.width, config.height, "10.0.0.0", false, flashvars, params);
+</script>
